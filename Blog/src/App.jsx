b5d01b2578a587bdad28d21c82aa9eb7dae4836b
iamsufiyan560 <140635114+iamsufiyan560@users.noreply.github.com>
2024-03-25 22:43:46 +0530

    add redux toolkit

--- Blog/src/App.jsx
+++ Blog/src/App.jsx
@@ -1,7 +1,36 @@
+import { useEffect, useState } from "react";
 import "./App.css";
+import { useDispatch } from "react-redux";
+import authService from "./appwrite/auth";
+import { login, logout } from "./store/authSlice";
+import { Footer, Header } from "./components/index";
 
 function App() {
-  return <div>hi</div>;
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .finally(() => setLoading(false));
+  }, []);
+
+  return !loading ? (
+    <div className="min-h-screen flex flex-wrap content-between bg-gray-400">
+      <div className="w-full block">
+        <Header></Header>
+        <main>TODO: {/* <Outlet /> */}</main>
+        <Footer></Footer>
+      </div>
+    </div>
+  ) : null;
 }
 
 export default App;
